99e50a067f5829b8837fb68dda9e3b09e2f1eb71
Homin <coc1381@naver.com>
2024-08-16 15:07:05 +0900

    fix: add link href in footer logo image

--- app/shared/components/footer/Footer.tsx
+++ app/shared/components/footer/Footer.tsx
@@ -1,44 +1,52 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  const handleLogoClick = () => {
-    alert("기능 개발중입니다.");
-  };
-
   return (
     <div className="flex items-center justify-between bg-linkbrary-navy px-8 pb-[64px] pt-8">
       <div className="text-linkbrary-gray_8">©codeit - 2024</div>
-      <div className="text-linkbrary-gray_9">
+      <div
+        className="cursor-pointer text-linkbrary-gray_9"
+        onClick={() => alert("기능 개발중입니다.")}
+      >
         <div>Privacy Policy</div>
         <div>FAQ</div>
       </div>
-      <div className="flex cursor-pointer space-x-3" onClick={handleLogoClick}>
-        <Image
-          src="/images/footer/facebook.svg"
-          alt="facebook"
-          width={20}
-          height={20}
-        />
-        <Image
-          src="/images/footer/twitter.svg"
-          alt="twitter"
-          width={20}
-          height={20}
-        />
-        <Image
-          src="/images/footer/youtube.svg"
-          alt="youtube"
-          width={20}
-          height={20}
-        />
-        <Image
-          src="/images/footer/instagram.svg"
-          alt="instagram"
-          width={20}
-          height={20}
-        />
+      <div className="flex cursor-pointer space-x-3">
+        <Link href="https://www.facebook.com/" target="_blank">
+          <Image
+            src="/images/footer/facebook.svg"
+            alt="facebook"
+            width={20}
+            height={20}
+          />
+        </Link>
+        <Link href="https://x.com/?lang=ko" target="_blank">
+          <Image
+            src="/images/footer/twitter.svg"
+            alt="twitter"
+            width={20}
+            height={20}
+          />
+        </Link>
+        <Link href="https://www.youtube.com/" target="_blank">
+          <Image
+            src="/images/footer/youtube.svg"
+            alt="youtube"
+            width={20}
+            height={20}
+          />
+        </Link>
+        <Link href="https://www.instagram.com/" target="_blank">
+          <Image
+            src="/images/footer/instagram.svg"
+            alt="instagram"
+            width={20}
+            height={20}
+          />
+        </Link>
       </div>
     </div>
   );
